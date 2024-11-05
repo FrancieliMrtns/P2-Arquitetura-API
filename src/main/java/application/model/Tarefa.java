303d911c0f0ba03d1512a1c384868f31df4a72b6
Francieli Martins <fmartinsPOW777@gmail.com>
2024-11-05 19:55:47 +0000

    model e DTO

---
+++ src/main/java/application/model/Tarefa.java
@@ -0,0 +1,39 @@
+package application.model;
+
+import java.time.LocalDate;
+
+//import application.record.TarefaDTO;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tarefa")
+public class Tarefa {
+    @Id
+    @Getter
+    @Setter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String titulo;
+    private String descricao;
+    @Column(nullable = false)
+    private LocalDate dataCriacao;
+    private LocalDate dataInicio;
+    private LocalDate dataConclusao; 
+
+    /*public Tarefa(TarefaDTO dados){
+        this.id = dados.id();
+        this.titulo = dados.titulo();
+        this.dataCriacao = dados.dataCriacao();
+        this.dataInicio = dados.dataInicio();
+        this.dataConclusao = dados.dataConclusao();
+
+    } */
+}
---
+++ src/main/java/application/record/TarefaDTO.java
@@ -0,0 +1,8 @@
+package application.record;
+
+import application.model.Tarefa;
+
+/*public record TarefaDTO(long id, String titulo, String descricao, LocalDate dataCriacao, LocalDate dataInicio, LocalDate dataConclusao ) {
+    public TarefaDTO
+    
+}*/
