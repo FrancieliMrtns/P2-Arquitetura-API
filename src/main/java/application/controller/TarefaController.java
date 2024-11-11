4a48a2a8ada268c0f73504da89df4303351e6ac9
Francieli Martins <fmartinsPOW777@gmail.com>
2024-11-11 19:26:59 +0000

    commit

--- src/main/java/application/controller/TarefaController.java
+++ src/main/java/application/controller/TarefaController.java
@@ -2,10 +2,10 @@ package application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import application.record.TarefaDTO;
 import application.service.TarefaService;
@@ -22,10 +22,23 @@ public class TarefaController {
         return tarefaSrv.findAll();
     }
 
+    /*@GetMapping("/{id}")
+    public TarefaDTO findOne(@PathVariable long id){
+        return tarefaSrv.findById(id);
+    }*/
+
     @PostMapping
     public TarefaDTO insert(@RequestBody TarefaDTO tarefa){
         return tarefaSrv.inserirTarefa(tarefa);
     } 
+
+    /*@PutMapping("/{id}")
+    public TarefaDTO update(@PathVariable long id, @RequestBody TarefaDTO tarefa){
+        return tarefaSrv.update(id, tarefa);
+    }*/
+
+
+
 
 
 
